1e6a6001ee56aaec55b91dfe5028a35c7e36f4ed
Ucheduk <dukweb4@gmail.com>
2021-09-07 15:55:51 +0100

    add selection sort algorithm

---
+++ selectionSort/index.js
@@ -0,0 +1,19 @@
+const selectionSort = (arr) => {
+  let indexOfMin;
+  for (let i = 0; i < arr.length; i++) {
+    indexOfMin = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[indexOfMin] > arr[j]) {
+        indexOfMin = j;
+      }
+    }
+    if (i !== indexOfMin) {
+      let temp = arr[i];
+      arr[i] = arr[indexOfMin];
+      arr[indexOfMin] = temp;
+    }
+  }
+  return arr;
+};
+
+console.log(selectionSort([67, -5, 3, 5, -46, 8, 2, 1, 4]));
